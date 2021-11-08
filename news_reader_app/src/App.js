80bc34a07a0b61bf1e4fb165aeefaa4cc42524c1
Dikshit Kaushal <dikshitkaushal49@gmail.com>
2021-11-08 13:59:18 +0530

    AlanAI imported

--- news_reader_app/src/App.js
+++ news_reader_app/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useEffect } from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web'
 function App() {
+  const alanKey='3eb79c1b6393e597e4416ed06a9002a02e956eca572e1d8b807a3e2338fdd0dc/stage'
+  useEffect(()=>{
+    alanBtn({
+      key:alanKey,
+      onCommand:({command})=>{
+        if(command==='testcommand'){
+          alert("Command Executed")
+        }
+      }
+    })
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Alan AI News Application</h1>
     </div>
   );
 }
